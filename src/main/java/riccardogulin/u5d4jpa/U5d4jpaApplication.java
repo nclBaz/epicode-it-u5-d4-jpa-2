f8041b39dabc6387f8fc9eabca2f045d7c1cadef
Riccardo Gulin <riccardo.gulin@gmail.com>
2023-07-27 11:23:56 +0200

    Adds basic structure

---
+++ src/main/java/riccardogulin/u5d4jpa/U5d4jpaApplication.java
@@ -0,0 +1,13 @@
+package riccardogulin.u5d4jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5d4jpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5d4jpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/riccardogulin/u5d4jpa/U5d4jpaApplicationTests.java
@@ -0,0 +1,13 @@
+package riccardogulin.u5d4jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5d4jpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
